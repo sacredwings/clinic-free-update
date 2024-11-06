30cb44000e2b982fdffb462eb4f42ff72f94ef3b
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2024-11-06 21:00:01 +0700

    Обновление 1

--- src/index.js
+++ src/index.js
@@ -13,57 +13,55 @@ async function handler(req, res) {
 
     //Пользователи
     let collectionUser = mongoClient.collection(`user`)
-    let arFields = {
+    let arFieldsUser = {
         login: 'test',
         password: '$2b$10$zdWLdO9rHZgTiTkPKQYsY.6TBAMH.pTt6MZcZcb7i85.2vCA7uUkq'
     }
-    await collectionUser.insertOne(arFields)
-    arFields = {
+    await collectionUser.insertOne(arFieldsUser)
+    arFieldsUser = {
         login: 'admin',
         password: '$2b$10$zdWLdO9rHZgTiTkPKQYsY.6TBAMH.pTt6MZcZcb7i85.2vCA7uUkq'
     }
-    await collectionUser.insertOne(arFields)
+    await collectionUser.insertOne(arFieldsUser)
 
     //Клинтка
     let collectionClinic = mongoClient.collection(`clinic`)
-    arFields = {
-        title: 'test',
+    let arFieldsClinic = {
+        title: 'Клиника здоровья',
         legal_name: 'ООО Пульсар',
+
         inn: '5446012122',
         kpp: '544601001',
         ogrn: '1105472000787',
 
-        create_user_id: new DB().ObjectID(arFields._id)
+        create_user_id: new ObjectId(arFieldsUser._id),
+        create_date: new Date()
     }
-    await collectionClinic.insertOne(arFields)
+    await collectionClinic.insertOne(arFieldsClinic)
 
-    //await collectionVideo.rename("video")
-    //await collectionImg.rename("img")
-    //await collectionAudio.rename("audio")
-    //await collectionDoc.rename("doc")
+    let collectionWorker = mongoClient.collection(`worker`)
+    await collectionWorker.rename("prof-examination")
+    await collectionWorker.updateMany({}, {$set: {clinic_id: new ObjectId(arFieldsClinic._id)}})
 
+    let collectionOrg = mongoClient.collection(`org`)
+    await collectionOrg.updateMany({}, {$set: {clinic_id: new ObjectId(arFieldsClinic._id)}})
 
-    //let result = await collection.find({to_group_id: new ObjectId('61a9fdb494f73f29366ecddf')}).toArray()
-    let result = await collection.find({dislike: null}).toArray()
-    console.log(result)
+    let collectionContract = mongoClient.collection(`contract`)
+    await collectionContract.updateMany({}, {$set: {clinic_id: new ObjectId(arFieldsClinic._id)}})
 
+    let collectionPrice = mongoClient.collection(`price`)
+    await collectionContract.updateMany({}, {$set: {clinic_id: new ObjectId(arFieldsClinic._id)}})
+
+    /*
+    let result = await collectionWorker.find({}).toArray()
     for (let item of result) {
         let arFields = {
-            //_id: item._id,
-            //from_id: new ObjectId('61a9fdb194f73f29366e9c22'),
-            dislike: false,
-            /*
-            to_user_id: item.to_user_id,
-            album_id: item.album_id,
-            title: item.title,
-            text: '',
-            image_id: item.image_id,*/
-
+            clinic_id: new DB().ObjectID(arFieldsClinic._id),
         }
 
-        let result = await collection.updateOne({_id: item._id}, {$set: arFields})
+        let result = await collectionWorker.updateOne({_id: item._id}, {$set: arFields})
         console.log(item)
-    }
+    }*/
 
 
 
